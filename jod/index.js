9c0af6896e083aaf61ffc0df7f5d6b40b948ba54
Melody Lee <melody.lee357@gmail.com>
2022-07-08 14:23:13 -0400

    foundational code

--- jod/index.js
+++ jod/index.js
@@ -1,23 +1,20 @@
-const fetch = require("node-fetch");
-
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const jokesEndpoint = "https://api.jokes.one/jod";
-
-    // should get it in the form of a json
-    let resp = await fetch(jokesEndpoint, {
-        "method": "GET",
-        headers: {
-            'Content-type': "application/json",
-            "X-JokesOne-Api-Secret": "melodyweek4"
+const getJoke = document.getElementById('getJoke');
+        const output = document.getElementById("output");
+        
+        async function returnJokes(event) {
+            event.preventDefault()
+            const jokesEndpoint = "https://api.jokes.one/jod";
+            
+            // should get it in the form of a json
+            let resp = await fetch(jokesEndpoint, {
+                "method": "GET",
+                headers: {
+                    "Content-type":"application/json"
+                }
+            });
+            const jsonData = await resp.json();
+            const joke = jsonData.contents.jokes[0].joke.text
+            output.textContent = joke
         }
-    });
-    
 
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: resp
-    };
-}
+        getJoke.addEventListener('click', returnJokes)
