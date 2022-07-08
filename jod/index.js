9d33585b8e6b9f8be50a1e1a6424bf3035956a91
Melody Lee <melody.lee357@gmail.com>
2022-07-08 16:08:46 -0400

    add comments

--- jod/index.js
+++ jod/index.js
@@ -1,12 +1,14 @@
+// get the elements
 const getJoke = document.getElementById("getJoke");
 const output = document.getElementById("output");
 const jokeTitle = document.getElementById("jokeTitle");
 
+// function that will get the jokes
 async function returnJokes(event) {
   event.preventDefault();
-  const jokesEndpoint = "https://api.jokes.one/jod";
+  const jokesEndpoint = "https://api.jokes.one/jod"; // limited ot 10 requests per hour
 
-  // should get it in the form of a json
+  // get response in form of json
   let resp = await fetch(jokesEndpoint, {
     method: "GET",
     headers: {
@@ -14,9 +16,15 @@ async function returnJokes(event) {
     },
   });
   const jsonData = await resp.json();
+
+  // get the joke and title, respectively
   const joke = jsonData.contents.jokes[0].joke.text;
-  jokeTitle.textContent = jsonData.contents.jokes[0].joke.title;
+  const title = jsonData.contents.jokes[0].joke.title;
+
+  // put joke and title into HTML
+  jokeTitle.textContent = title;
   output.textContent = joke;
 }
 
+// create event listener (triggered by click of button)
 getJoke.addEventListener("click", returnJokes);
