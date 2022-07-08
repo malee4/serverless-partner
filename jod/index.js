262f4df496809320f66f9eb4694ea9d3ed4a9aa9
Melody Lee <melody.lee357@gmail.com>
2022-07-08 14:33:39 -0400

    add display title

--- jod/index.js
+++ jod/index.js
@@ -1,20 +1,23 @@
-const getJoke = document.getElementById('getJoke');
-        const output = document.getElementById("output");
-        
-        async function returnJokes(event) {
-            event.preventDefault()
-            const jokesEndpoint = "https://api.jokes.one/jod";
-            
-            // should get it in the form of a json
-            let resp = await fetch(jokesEndpoint, {
-                "method": "GET",
-                headers: {
-                    "Content-type":"application/json"
-                }
-            });
-            const jsonData = await resp.json();
-            const joke = jsonData.contents.jokes[0].joke.text
-            output.textContent = joke
-        }
+const getJoke = document.getElementById("getJoke");
+const output = document.getElementById("output");
+const jokeTitle = document.getElementById("jokeTitle");
 
-        getJoke.addEventListener('click', returnJokes)
+async function returnJokes(event) {
+  event.preventDefault();
+  const jokesEndpoint = "https://api.jokes.one/jod";
+
+  // should get it in the form of a json
+  let resp = await fetch(jokesEndpoint, {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  const jsonData = await resp.json();
+  const joke = jsonData.contents.jokes[0].joke.text;
+  const title = jsonData.contents.jokes[0].title;
+  jokeTitle.textContent = title;
+  output.textContent = joke;
+}
+
+getJoke.addEventListener("click", returnJokes);
